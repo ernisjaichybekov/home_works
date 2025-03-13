c1fd9ccb92fe4d32032b6fb28577633f5ca8c0bb
ernisjaichybekov <ernis00700708@gmail.com>
2025-03-13 20:09:11 +0600

    Set tibi

---
+++ home_work.py
@@ -0,0 +1,16 @@
+
+nums = {1,1,1,2,2,2,3,3,3}
+names = {
+    "Arsen",
+    "Grad",
+    "kanat",
+    "ernis",
+    "shergazy",
+    "Aida",
+    "aiana",
+    "Begimay"
+}
+name = input("Enter a name: ")
+print(names)
+names.add(name)
+print(names)
