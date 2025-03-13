3588d92a3378f9fc2b395ec8abee6686e7f9461d
ernisjaichybekov <ernis00700708@gmail.com>
2025-03-13 19:27:55 +0600

    ui tap

---
+++ home_work.py
@@ -0,0 +1,48 @@
+# a = 20
+# if a % 2 == 0:
+#     print("jup")
+# else:
+#     print("tak")
+#
+# print("jup" if a % 2 == 0 else "Tak ")
+# [
+# print(
+#     ['jup' if i % 2 == 0 else 'tak'
+#      for i in range(int(input())+1)]]
+# )
+#
+#
+#
+# print("* 1 *")
+# n = [i for i in range(1,21)]
+# print(n)
+#
+# print("* 2 * ")
+# res = [i ** 2 for i in range (1, 11)]
+# print(res)
+#
+# print("* 3 *")
+# numbers = [5, 12, 7, 18, 3, 10, 8]
+# res = [n for n in numbers if n > 7]
+# print(res)
+
+# print("* 4 *")
+# n = ["яблоко", "банан", "вишня"]
+# number = [i for  i in  n]
+# print(number)
+
+
+# print("* 5 *")
+# res = int(input("san jaz: "))
+# number = "on san" if res > 0 else "ters san"
+# print(number)
+
+# print("* 6 *")
+# n = int(input("san jaz: "))
+# res = "jup san" if n % 2 == 0 else "tak san"
+# print(res)
+
+# print("* 7 *")
+# number = [4, -1, 7, -3, 0, 9, -2]
+# res= [i if i >= 0 else 0 for i in number]
+# print(res)
