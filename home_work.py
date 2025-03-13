ce818c882b59bd51ae0bfcb3867e356cb25a2f1c
ernisjaichybekov <ernis00700708@gmail.com>
2025-03-13 21:00:08 +0600

    taper

---
+++ home_work.py
@@ -0,0 +1,28 @@
+# def func():
+#     print("python")
+#     print("java")
+#     print("c++")
+#     print("#" * 10)
+#
+# def area_circle():
+#     radius = float(input("Enter the radius: "))
+#     s = 3.14 * radius * 2
+#     print("aiat = ",s)
+#
+#
+# def area_rectangle(width,height):
+#     if width < 0 or height < 0:
+#         print("san jaz: ")
+#     else:
+#      area = width * height
+#      print("aiat = ",area)
+# #
+# area_circle()
+# area_rectangle(10,11)
+
+def a_n(name):
+  print(f'Your name is {name}')
+
+  aty = input("Enter your name: ")
+  a_n(aty)
+
