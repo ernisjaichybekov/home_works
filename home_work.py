99d5745256f90126a42e3e1de93645e6eefecb41
ernisjaichybekov <ernis00700708@gmail.com>
2025-03-15 19:36:18 +0600

    jaz

--- home_work.py
+++ home_work.py
@@ -19,10 +19,38 @@
 # #
 # area_circle()
 # area_rectangle(10,11)
+#
+# def a_n(name):
+#   print(f'Your name is {name}')
+#
+#   aty = input("Enter your name: ")
+#   a_n(aty)
 
-def a_n(name):
-  print(f'Your name is {name}')
+# 1
+def greet():
 
-  aty = input("Enter your name: ")
-  a_n(aty)
+    print("Салам, колдонуучу!")
+greet()
+greet()
 
+#2
+def weather():
+    print("Бүгүн 15°C")
+weather()
+
+#3
+def favorite_food():
+    print("manti")
+favorite_food()
+
+#4
+def sum_numbers():
+    print(f"7 + 3 = 10")
+sum_numbers()
+
+# 5
+def repeat_phrase():
+    print("Окуу — сонун нерсе!")
+repeat_phrase()
+repeat_phrase()
+repeat_phrase()
---
+++ return.py
@@ -0,0 +1,16 @@
+def add (a,b):
+    print(a + b)
+    return a +b
+
+res =add (1,2)
+print(res)
+
+def wither(celsius):
+    if celsius < 0 :
+        return "aba cyyk"
+    elif 0 < celsius < 20:
+        return "aba jylyy"
+    else:
+        return "aba iccik"
+print(wither(25))
+
