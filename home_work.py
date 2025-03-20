19f160960dafa805de214de2d117086de9dd0f3a
ernisjaichybekov <ernis00700708@gmail.com>
2025-03-20 18:14:42 +0600

    erorr

---
+++ home_work.py
@@ -0,0 +1,39 @@
+# try:
+#     a = int(input())
+#     b = int(input())
+#
+#     c = a / b
+#     print(c)
+# except ZeroDivisionError:
+#     print("candi nolgo bolgondo bolboit!")
+# except ValueError:
+#     print("texcti canra ailandirganga bolboit")
+# except Exception:
+#     print("kata bar")
+#
+# def is_date(day,month,year):
+#     from datetime import date
+#     try:
+#         return date (year,month,day)
+#     except ValueError:
+#         return ("kalendar mindai date jok!")
+#
+# print(is_date(18,3,2025))
+# # print(is_date(29,2,2100))
+# number[1,2,3,4,5]
+#
+# def det_element():
+#     try:
+#      index = int(input("Enter a the "))
+#      return number[index]
+#     except IndexError:
+#         print("mindai index jok")
+
+
+
+# try:
+#     number = int(input("san jaz: "))
+#     print(f" ciz {number} canin jazdiniz")
+# except ValueError:
+#     print("bul san emes! can jaz. ")
+
